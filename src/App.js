93674504d345edd1e8753b386a32b4ff0a5db5e9
Andrew Wilson <anwils2@pdx.edu>
2022-05-10 18:02:33 -0700

    actually add bootstrap; add bkg

--- src/App.js
+++ src/App.js
@@ -1,15 +1,28 @@
 import "./App.css";
 import TextInput from "./components/input";
 
+const backgroundsPath = process.env.PUBLIC_URL + "/backgrounds/";
+
 function App() {
   return (
     <div>
       <header>
         <h2>Handout Maker</h2>
       </header>
-      <div>
-        <TextInput />
-        <TextInput />
+      <div className="container">
+        <div className="row">
+          <div className="bkg-select col">
+            <img src={`${backgroundsPath}pamphlet.jpg`} />
+          </div>
+          <div className="row">
+            <div className="col">
+              <TextInput />
+            </div>
+            <div className="col">
+              <TextInput />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
